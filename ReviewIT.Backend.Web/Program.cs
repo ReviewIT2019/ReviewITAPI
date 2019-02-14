2790b5c1a6d55615c8ced37e1485232d09f6b979
Jonas Anker Rasmussen <jonasanker@gmail.com>
2019-02-14 11:42:20 +0100

    Renamed workspace to ReviewIT.Backend.*
    Changed .sln files, .csproj files and travis and azure pipelines config

--- ReviewIT.Backend.Web/Program.cs
+++ ReviewIT.Backend.Web/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace ReviewITAPI
+namespace ReviewIT.Backend.Web
 {
     public class Program
     {
