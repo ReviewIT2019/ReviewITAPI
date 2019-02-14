74c746bd634913b33023f11d3d8a5e810b384bf7
Jonas Anker Rasmussen <jonasanker@gmail.com>
2019-02-14 18:26:32 +0100

    Setup spike w Study Entity, Repository and Controller

---
+++ ReviewIT.Backend.Entities/Contexts/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewIT.Backend.Entities.Entities;
+using System.Configuration;
+
+namespace ReviewIT.Backend.Entities.Contexts
+{
+    public class Context : DbContext, IContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Study> Studies { get; set; }
+    }
+}
---
+++ ReviewIT.Backend.Entities/Contexts/IContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewIT.Backend.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReviewIT.Backend.Entities.Contexts
+{
+    public interface IContext : IDisposable
+    {
+        DbSet<Study> Studies { get; set; }
+    }
+}
