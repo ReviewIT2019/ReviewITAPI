c2eef63e700f9e4da8b4b855219ecf887c4ba06e
Jonas Anker Rasmussen <jonasanker@gmail.com>
2019-02-16 22:51:26 +0100

    Stage entity added w/ Migrations

--- ReviewIT.Backend.Entities/Contexts/Context.cs
+++ ReviewIT.Backend.Entities/Contexts/Context.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ReviewIT.Backend.Entities.Entities;
+using ReviewIT.Backend.Entities.Entitities;
 
 namespace ReviewIT.Backend.Entities.Contexts
 {
     public class Context : DbContext, IContext
     {
+        public Context()
+        {
+        }
+
         public Context(DbContextOptions<Context> options)
             : base(options)
         {
         }
 
         public virtual DbSet<Study> Studies { get; set; }
+        public virtual DbSet<Stage> Stages { get; set; }
     }
 }
---
+++ ReviewIT.Backend.Entities/Migrations/20190216213902_StageEntity.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReviewIT.Backend.Entities.Migrations
+{
+    public partial class StageEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 256, nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    StageInitiated = table.Column<bool>(nullable: false),
+                    StudyId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Stages_Studies_StudyId",
+                        column: x => x.StudyId,
+                        principalTable: "Studies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stages_StudyId",
+                table: "Stages",
+                column: "StudyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stages");
+        }
+    }
+}
