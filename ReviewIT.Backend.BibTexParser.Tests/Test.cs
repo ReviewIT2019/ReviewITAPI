c26310545fb39f5fa99c53bfe96aedafaf2260a5
niss <niss>
2019-02-16 17:52:48 +0100

    added dummy test to rule out error in sonarqube

---
+++ ReviewIT.Backend.BibTexParser.Tests/Test.cs
@@ -0,0 +1,21 @@
+﻿using ReviewIT.Backend.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ReviewIT.Backend.BibTexParser.Tests
+{
+    public class Test
+    {
+        [Fact(DisplayName = "Test")]
+        public async Task Get_returns_Ok_with_studies()
+        {
+
+            // Assert
+            Assert.Equal("", string.Empty);
+        }
+
+    }
+}
